8a6fad240ed45fac3b1825d9af16b7b0685901c2
Syed Areeb Shah <syedareeb@gsoftconsulting.com>
2022-06-24 12:05:30 +0500

    agora installed and bug removed

--- App.tsx
+++ App.tsx
@@ -7,6 +7,8 @@ import {
   Text,
   useColorScheme,
   View,
+  PermissionsAndroid,
+  Permission
 } from 'react-native';
 import StackNavigation from './app/Navigation/StackNav';
 import {NavigationContainer} from '@react-navigation/native';
@@ -17,6 +19,23 @@ import CallingScreen from './app/screens/CallingScreen/CallingScreen';
 
 
 const App = () => {
+  const requestCameraAndAudioPermission = async () => {
+    try {
+      const granted = await PermissionsAndroid.requestMultiple([
+        PermissionsAndroid.PERMISSIONS.RECORD_AUDIO,
+      ]);
+      if (
+        granted['android.permission.RECORD_AUDIO'] ===
+        PermissionsAndroid.RESULTS.GRANTED
+      ) {
+        console.log('You can use the mic');
+      } else {
+        console.log('Permission denied');
+      }
+    } catch (err) {
+      console.warn(err);
+    }
+  };
   return (
     <ThemeProvider>
       <NavigationContainer>
